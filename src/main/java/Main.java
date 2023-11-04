9e7cbed3066f7526d71831d97b752aabac860597
PeterBurrfot <arr.in.the.pw@gmail.com>
2023-11-04 13:43:49 +0300

    PrimeNumber

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         int verificationNum = 3;
-        boolean prost = true;
+        boolean primeNum = true;
         for (int i = 2; i < verificationNum; i++) {
-            if (verificationNum % i == 0)
-                prost = false;
+            if (verificationNum % i == 0) {
+                primeNum = false;
+            }
         }
-        if (prost) System.out.println("Число " + verificationNum + " является простым");
-        else System.out.println("Число " + verificationNum + " является составным");
+        if (primeNum) {
+            System.out.println("Число " + verificationNum + " является простым");
+        } else {
+            System.out.println("Число " + verificationNum + " является составным");
+        }
     }
 }
